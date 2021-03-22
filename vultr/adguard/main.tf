04d938f203fc2ca01a5a8aa4eef1d9e9d5075223
Dien Phan <ptdien.lxag@gmail.com>
2021-03-22 10:54:22 +0700

    Update main.tf

--- vultr/adguard/main.tf
+++ vultr/adguard/main.tf
@@ -1,20 +1,45 @@
 terraform {
   required_providers {
     vultr = {
-      source = "vultr/vultr"
+      source  = "vultr/vultr"
       version = "2.1.3"
     }
   }
+  backend "remote" {
+
+    hostname     = "app.terraform.io"
+    organization = "dienp"
+    workspaces {
+      name = "terraform-infras"
+    }
+
+  }
+}
+
+variable "vultr_api_key" {
+  type = string
 }
 
 # Configure the Vultr Provider
 provider "vultr" {
-  api_key = "CTFX6OEIW6DHCOULWKPYWMWRICRYIE66J3KQ"
-  rate_limit = 700
+  api_key     = var.vultr_api_key
+  rate_limit  = 700
   retry_limit = 3
 }
 
-# Create a web instance
-resource "vultr_instance" "web" {
-    
+resource "vultr_user" "admin" {
+  name        = "admin"
+  email       = "user@vultr.com"
+  password    = "myP@ssw0rd"
+  api_enabled = true
+  acl = [
+    "manage_users",
+    "subscriptions",
+    "provisioning",
+    "billing",
+    "support",
+    "abuse",
+    "dns",
+    "upgrade",
+  ]
 }
